df4c2995404e82df4753fa856a1820bf5b2b5b24
ddsh <haoogame@gmail.com>
2019-03-16 22:40:40 +0300

    поправил @global в документации

--- 0/errorHandling.c
+++ 0/errorHandling.c
@@ -9,6 +9,7 @@
  * Функция, обрабатывающая ошибки и выводящая сообщения о них на экран.
  * @param double a -- первое введённое число
  * @param int* err -- указатель на номер ошибки из главной функции
+ * @global err возвращается в 0 в конце функции.
  * @example handleError(-1); // printf("DIVISION BY ZERO");
  * */
 void handleError(double a, int* err){
